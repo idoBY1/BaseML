aa8b848ffd40202bf5b496683adabe66f818b67a
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2024-12-29 14:38:50 +0200

    created matrix class

--- include/matrix.h
+++ include/matrix.h
@@ -1,6 +1,101 @@
 #pragma once
 
+#include <algorithm> // Needed for std::copy
+
 namespace MachineLearning
 {
-	
+    template<typename T>
+    class Matrix
+    {
+    private:
+        size_t rows, cols;
+        T* data;
+
+    public:
+        Matrix(size_t numOfRows, size_t numOfCols);
+        ~Matrix();
+
+        Matrix(const Matrix& other); // Copy contructor
+        Matrix(Matrix&& other) noexcept; // Move constructor
+
+        // operators overload (operators are invoked when changing an object that already exists)
+        Matrix& operator=(const Matrix& other); // Copy operator
+        Matrix& operator=(Matrix&& other) noexcept; // Move operator
+
+        T& operator()(size_t row, size_t col); // access operator
+        const T& operator()(size_t row, size_t col) const; // const access operator
+    };
+
+    // --- IMPLEMENTATION ---
+    template<typename T>
+    Matrix<T>::Matrix(size_t numOfRows, size_t numOfCols)
+        : rows(numOfRows), cols(numOfCols)
+    {
+        data = new T[rows * cols];
+    }
+
+    template<typename T>
+    Matrix<T>::~Matrix()
+    {
+        delete[] data;
+    }
+
+    template<typename T>
+    Matrix<T>::Matrix(const Matrix<T>& other)
+        : rows(other.rows), cols(other.cols)
+    {
+        data = new T[rows * cols];
+        std::copy(other.data, other.data + (rows * cols), data);
+    }
+
+    template<typename T>
+    Matrix<T>::Matrix(Matrix<T>&& other) noexcept
+        : rows(other.rows), cols(other.cols), data(other.data)
+    {
+        other.data = nullptr; // Nullify the pointer to avoid double deletion
+    }
+
+    template<typename T>
+    Matrix<T>& Matrix<T>::operator=(const Matrix<T>& other)
+    {
+        if (this != &other) {
+            delete[] data; // Free existing memory
+
+            rows = other.rows;
+            cols = other.cols;
+            data = new T[rows * cols]; // Allocate new memory
+
+            std::copy(other.data, other.data + (rows * cols), data);
+        }
+
+        return *this;
+    }
+
+    template<typename T>
+    Matrix<T>& Matrix<T>::operator=(Matrix<T>&& other) noexcept
+    {
+        if (this != &other) {
+            delete[] data; // Free existing memory
+
+            rows = other.rows;
+            cols = other.cols;
+            data = other.data;
+
+            other.data = nullptr; // Nullify the pointer to avoid double deletion
+        }
+
+        return *this;
+    }
+
+    template<typename T>
+    T& Matrix<T>::operator()(size_t row, size_t col)
+    {
+        return data[row * cols + col];
+    }
+
+    template<typename T>
+    const T& Matrix<T>::operator()(size_t row, size_t col) const
+    {
+        return data[row * cols + col];
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
+#include "matrix.h"
+
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	MachineLearning::Matrix<float> mat(3, 2);
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			mat(i, j) = i * 2 + j;
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			std::cout << mat(i, j) << "\n";
+		}
+	}
 
 	return 0;
 }
