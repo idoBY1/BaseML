39796e72df90cff6aa95937c7e424f59fd306545
idoBY1 <94797559+idoBY1@users.noreply.github.com>
2025-02-14 14:47:45 +0200

    added virtual functions to IEnvironment

--- include/IEnvironment.h
+++ include/IEnvironment.h
@@ -8,6 +8,28 @@ namespace BaseML
 	{
 		// Update the environment's state. 'deltaTime' is the amount of time to assume that 
 		// have passed since the last update.
-		virtual void update(float deltaTime);
+		virtual void update(float deltaTime) = 0;
+
+		// Get the current state of the player as a column matrix.
+		virtual const Matrix& getState(const char* playerId) = 0;
+
+		// Set the action that the player will perform in the next update.
+		virtual void setAction(const char* playerId, Matrix action) = 0;
+
+		// Get the reward of the current state of the player.
+		virtual float getReward(const char* playerId) = 0;
+
+		// Initialize the environment.
+		virtual void initialize() = 0;
+
+		// Closes the environment's resources.
+		virtual void close() = 0;
+
+		// Resets the environment back to the initial state (this function expects the environment 
+		// to be already initialized).
+		virtual void reset() = 0;
+
+		// Display the current state of the environment.
+		virtual void render() = 0;
 	};
 }
